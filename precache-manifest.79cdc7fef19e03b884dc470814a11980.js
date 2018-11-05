8d0c40fabed7073c60ec02ce9f9deb7dd5f2c6f2
ABE1112 <Abraham.Elie@lc.cuny.edu>
2018-11-05 14:58:56 -0500

    Updates

---
+++ precache-manifest.79cdc7fef19e03b884dc470814a11980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e97240c72508ccae1d4",
+    "url": "/ReactPages/static/js/runtime~main.1e97240c.js"
+  },
+  {
+    "revision": "958102cdf001c2cae796",
+    "url": "/ReactPages/static/js/main.958102cd.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/ReactPages/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "958102cdf001c2cae796",
+    "url": "/ReactPages/static/css/main.0a487798.chunk.css"
+  },
+  {
+    "revision": "e6614d9c6c412f58c0f0809dd392fd5e",
+    "url": "/ReactPages/index.html"
+  }
+];
